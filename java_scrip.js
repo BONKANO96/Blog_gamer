94ce7ebba07b62e2c4726129af6e2420a61f0322
BONKANO Abdoul <bonkano69@gmail.com>
2024-04-29 06:23:02 +0200

    Mise_a_jour

---
+++ java_scrip.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.social-navigation').classList.toggle('active');
+});
+
+document.querySelector('.joueur').addEventListener('click', function() {
+    document.querySelector('.connexion').classList.toggle('active');
+});
